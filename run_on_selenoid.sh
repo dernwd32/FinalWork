64a24fdc56db6052b088d63ad2b33d2b99f61429
dernwd <dernwd32@gmail.com>
2025-03-01 12:54:39 +0300

    1.9 remote run on selenoid

---
+++ run_on_selenoid.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+REMOTE_URL=""
+BROWSER_NAME=""
+BROWSER_VERSION=""
+
+while [[ $# -gt 0 ]];
+do
+	case $1 in
+		--remote.url)
+			REMOTE_URL=$2
+			shift 2
+			;;
+		--browser.name)
+			BROWSER_NAME=$2
+			shift 2
+			;;
+		--browser.version)
+			BROWSER_VERSION=$2
+			shift 2
+			;;
+		*)
+			echo "Unknown argument: $1 with value $2"
+			exit 13
+			;;
+	esac
+done
+
+echo "Running UI tests on selenoid via maven"
+echo "~~ Selenoid URL: $REMOTE_URL"
+echo "~~ Chosen browser: $BROWSER_NAME ~ $BROWSER_VERSION"
+mvn clean test -Dremote.url=$REMOTE_URL -Dbrowser.name=$BROWSER_NAME -Dbrowser.version=BROWSER_VERSION
+
+sleep 100
+
--- src/main/java/webdriver/WebDriverFactory.java
+++ src/main/java/webdriver/WebDriverFactory.java
@@ -8,12 +8,27 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 
 public class WebDriverFactory implements IWebDriver{
     private WebDriver driver;
+
+    //http://193.104.57.173/wd/hub
+    private String remoteUrl = System.getProperty("remote.url");
+    private String browserName = System.getProperty("browser.name", "chrome");
+    private String browserVersion = System.getProperty("browser.version", "128.0");
+
+
 
     @Override
     public WebDriver create(String webDriverName) {
@@ -22,6 +37,20 @@ public class WebDriverFactory implements IWebDriver{
 
     @Override
     public WebDriver create(String webDriverName, String mode)  {
+
+        if (!Objects.isNull(remoteUrl)) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability(CapabilityType.BROWSER_NAME, browserName);
+            Map<String, Object> options = new HashMap<>();
+            options.put(CapabilityType.BROWSER_VERSION, browserVersion);
+            options.put("enableVNC", true);
+            capabilities.setCapability("selenoid:options", options);
+            try {
+                return new RemoteWebDriver(new URL(remoteUrl), capabilities);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
         boolean argsWasSet = (!mode.isEmpty() && mode.charAt(0) == '-');
 
